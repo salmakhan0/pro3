eb688bad6db2fe56776fc45a65a295ce15b843a3
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-26 19:02:44 +0600

    Add files via upload

---
+++ program28.js
@@ -0,0 +1,14 @@
+var h2 = document.getElementById("heading2");
+
+h2.innerHTML = "I love Bangladesh";
+h2.id = "heading2-new";
+console.log(h2);
+
+var a = document.querySelector("a");
+console.log(a)
+a.textContent="visit my website";
+a.href="http://www.studywithanis.com"
+
+a.style.textDecoration = "none";
+a.style.color = "green";
+a.style.fontSize = "2rem";
