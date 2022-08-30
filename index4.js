1a93e79c8e1ec58d39df93d3ea3e4d27672beac1
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-31 00:40:44 +0600

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("input");
+const p = document.querySelector("p");
+
+input.addEventListener("copy", function () {
+  p.innerText = "you have copied";
+});
+input.addEventListener("cut", function () {
+  p.innerText = "you have cut";
+});
+input.addEventListener("paste", function () {
+  p.innerText = "you have pasted";
+});
