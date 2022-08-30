3c815d947657e08f152d285d70eb3f535885e243
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-31 00:52:27 +0600

    Add files via upload

---
+++ index5.js
@@ -0,0 +1,7 @@
+
+const input = document.querySelector("input");
+input.addEventListener("blur", function (e) {
+  input.value = e.target.value.toUpperCase();
+});
+input.addEventListener("focus", function () 
+});
